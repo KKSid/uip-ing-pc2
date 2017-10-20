a30f4b475444c36a12dfccfe835de9167d0a4dd9
KKSid <32003435+KKSid@users.noreply.github.com>
2017-10-20 16:47:38 -0600

    Add files via upload

---
+++ Cosas/src/Ejercicio.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio {
+
+    public static void main (String[]args) throws IOException {
+        String Elec="S";
+        int cont = 0;
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        do{
+            String nombre="";
+            String tipo;
+            double venta=0;
+            double comision=0;
+            int selec = 0;
+
+            System.out.println("NoEstoyEntendiendo - Módulo de ventas");
+            System.out.print("Vendedor: ");
+            nombre = sc.readLine();
+            System.out.print("Tipo(A/Z): ");
+            tipo = sc.readLine();
+            System.out.print("VENTA MENSUAL: ");
+            venta = Double.parseDouble(sc.readLine());
+            if (tipo == "A" || tipo == "a"){
+                selec=1;
+            }
+            else {
+                selec=2;
+            }
+
+              switch (selec){
+
+                case 1:
+                        if (venta > 9999.99) {
+                            comision = venta * 0.15;
+
+                            System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+
+                        } else if (4999.99 < venta && venta < 9999.99) {
+                            comision = venta * 0.10;
+
+                            System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+
+                        }else if (999.99 >= venta || venta >= 4999.99) {
+                            comision = venta * 0.05;
+
+                            System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+
+                        } else {
+                            comision = venta * 0.02;
+
+                            System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+
+
+
+                }
+                break;
+
+                case 2:
+                    if (venta > 5000) {
+                        comision = venta * 0.07;
+
+                        System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+                    } else {
+                        comision = 0;
+                        System.out.println(nombre + " ," + " del tipo " + tipo + ", con venta mensual $" + venta + ", tiene comision de $" + comision);
+                    }
+                break;
+
+
+
+            }
+            System.out.println("Continuar S/N");
+            Elec = sc.readLine();
+            if (Elec == "N" || Elec == "n"){
+                cont = ++cont;
+                break;
+        }
+            else {
+                cont = 0;
+            }
+        }while (cont == 0);
+    }
+
+    }
